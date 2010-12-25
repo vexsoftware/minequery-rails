2a22eef16a91d71f06dc9ff9990627af3ba23e29
Kramer Campbell <kramer@kramerc.com>
2010-12-24 22:06:40 -0800

    Switched from using an Array to a Hash.

--- lib/minequery.rb
+++ lib/minequery.rb
@@ -1,33 +1,33 @@
 module Minequery
   module Methods
     def query_server(address, port, timeout = 30)
-      server = Array.new
-
       begin
         timeout(timeout) do
           query = TCPSocket.new(address, port)
           query.puts "QUERY"
+
+          server_port, player_count, player_list = nil
 
           while line = query.gets
             line_bits = line.split(" ", 2)
 
             case line_bits[0]
               when "SERVERPORT" then
-                server.push(line_bits[1].chomp)
+                server_port = line_bits[1].chomp
               when "PLAYERCOUNT" then
-                server.push(line_bits[1].chomp)
+                player_count = line_bits[1].chomp
               when "PLAYERLIST" then
-                server.push(line_bits[1].chomp[1..-2].split(", "))
+                player_list = line_bits[1].chomp[1..-2].split(", ")
             end
           end
 
           query.close
+
+          return { :server_port => server_port, :player_count => player_count, :player_list => player_list }
         end
       rescue Exception
-        server = nil
+        return nil
       end
-
-      server
     end
   end
 end
